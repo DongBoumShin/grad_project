8d243c4e2e0894bbc1f112d570608c134ce9eee5
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2023-11-12 17:14:58 +0900

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-jrt1k4-v8=yg5hda+bpm5b$f=(lp7ptq@68d%#_+66!zt1tgdd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+]
 
 
 # Application definition
