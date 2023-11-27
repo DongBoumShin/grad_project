52d0e02f3e74acdc5e433fd9ab92e91009855f4a
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2023-11-27 15:07:32 +0900

    need to add self ip to allowed hosts?

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com",
     "google.com",
-    "ip-172-31-35-135:8080",
+    "172-31-35-135",
     ".murphyproject.store",
     "127.0.0.1",
 ]
